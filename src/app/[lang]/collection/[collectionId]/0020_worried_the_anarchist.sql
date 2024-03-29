667b78019cecee194094d1acf2337168ba5d916b
Azamatbek <azamatbek.dev@gmail.com>
2024-03-29 16:10:14 +0500

    delete sql files

--- src/app/[lang]/collection/[collectionId]/0020_worried_the_anarchist.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "icollect_user" ALTER COLUMN "status" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "icollect_user" ALTER COLUMN "isAdmin" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "icollect_user" ALTER COLUMN "createdAt" SET NOT NULL;--> statement-breakpoint
--- src/app/[lang]/collection/[collectionId]/item/0021_fancy_goliath.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "icollect_item" ADD COLUMN "fts" tsvector;
-UPDATE "icollect_item" SET search_vector = to_tsvector('simple', 'name' || ' ' || custom_string1 || ' ' || custom_string2 || ' ' || custom_string3 || ' ' || custom_text1 || ' ' || custom_text2 || ' ' || custom_text3) stored;
-
-CREATE INDEX IF NOT EXISTS "icollect_item_fts_idx" ON "icollect_item" USING gin("fts");
